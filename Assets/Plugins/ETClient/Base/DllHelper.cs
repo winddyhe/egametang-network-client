6654c76ec0f1c2b67c3dcfaa4cf728bd96d360c0
winddyhe <hgplan@126.com>
2017-11-14 15:38:48 +0800

    First build, make it compile correct.

---
+++ Assets/Plugins/ETClient/Base/DllHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Model
+{
+	public static class DllHelper
+	{
+		public static Assembly LoadHotfixAssembly()
+		{
+			GameObject code = (GameObject)Resources.Load("Code");
+			byte[] assBytes = code.Get<TextAsset>("Hotfix.dll").bytes;
+			byte[] mdbBytes = code.Get<TextAsset>("Hotfix.mdb").bytes;
+			Assembly assembly = Assembly.Load(assBytes, mdbBytes);
+			return assembly;
+		}
+
+		public static Type[] GetHotfixTypes()
+		{
+            return new Type[0];
+
+            //if (ObjectEvents.Instance.HotfixAssembly == null)
+            //{
+            //    return new Type[0];
+            //}
+            //return ObjectEvents.Instance.HotfixAssembly.GetTypes();
+        }
+
+        public static Type[] GetMonoTypes()
+		{
+			List<Type> types = new List<Type>();
+			//foreach (Assembly assembly in ObjectEvents.Instance.GetAll())
+			//{
+			//	types.AddRange(assembly.GetTypes());
+			//}
+			return types.ToArray();
+		}
+	}
+}
---
+++ Assets/Plugins/ETClient/Network/OpcodeTypeComponent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Model
+{
+	public class OpcodeTypeComponent : MonoBehaviour
+	{
+        public long Id { get; set; }
+		private readonly DoubleMap<ushort, Type> opcodeTypes = new DoubleMap<ushort, Type>();
+
+		public void Awake()
+		{
+			Type[] monoTypes = DllHelper.GetMonoTypes();
+			foreach (Type monoType in monoTypes)
+			{
+				object[] attrs = monoType.GetCustomAttributes(typeof(MessageAttribute), false);
+				if (attrs.Length == 0)
+				{
+					continue;
+				}
+
+				MessageAttribute messageAttribute = attrs[0] as MessageAttribute;
+				if (messageAttribute == null)
+				{
+					continue;
+				}
+
+				this.opcodeTypes.Add(messageAttribute.Opcode, monoType);
+			}
+		}
+
+		public ushort GetOpcode(Type type)
+		{
+			return this.opcodeTypes.GetKeyByValue(type);
+		}
+
+		public Type GetType(ushort opcode)
+		{
+			return this.opcodeTypes.GetValueByKey(opcode);
+		}
+
+		public void Dispose()
+		{
+			if (this.Id == 0)
+			{
+				return;
+			}
+		}
+	}
+}

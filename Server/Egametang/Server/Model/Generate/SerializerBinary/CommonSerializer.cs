542af48c4779d53de2a824d13a1b08944bb0b883
winddyhe <hgplan@126.com>
2017-11-16 14:31:06 +0800

    Add windjson and serializer lib into server.

---
+++ Server/Egametang/Server/Model/Generate/SerializerBinary/CommonSerializer.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System.Collections.Generic;
+using Core;
+using Core.Serializer;
+
+
+/// <summary>
+/// 文件自动生成无需又该！如果出现编译错误，删除文件后会自动生成
+/// </summary>
+namespace Game.Knight
+{
+	public static class CommonSerializer
+	{
+	}
+}
